d30475acfce2e482aca2ded5634d36f0e788faac
Luis <luisito453@gmail.com>
2016-04-30 10:51:49 -0500

    package parties

---
+++ packages/parties/package.js
@@ -0,0 +1,16 @@
+Package.describe({
+  name: 'app:parties',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.3.2.4');
+  
+});
