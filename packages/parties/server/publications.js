a02b048dc507416c877748407791621761f3a8db
Luis <luisito453@gmail.com>
2016-05-01 09:36:46 -0500

    No technique is forbidden.

--- packages/parties/server/publications.js
+++ packages/parties/server/publications.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Parties, Summoners } from '../collection';
 
+// This is the data we send to the client
 Meteor.publish('party', function (partyId) {
 	const party = Parties.find({_id: partyId});
 	if (party.count()) {
