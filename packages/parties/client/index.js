3cfbe28eb0396e805b02f64656ef6c575abca6a3
Julian Alvarez <j@pobladopropiedades.com>
2016-04-30 22:06:18 -0500

    added startup page, now is looking good, is exciting :D, goluis do your logic magic

--- packages/parties/client/index.js
+++ packages/parties/client/index.js
@@ -1,6 +1,7 @@
-import './global.css';
-import './web.css';
 import './main_layout.html';
 import './home.html';
 import './start.html';
+import './start.js';
 import '../routes.js';
+import './global.css';
+import './web.css';
